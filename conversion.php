d14e1d3b86a4cda6998a0536fd2105770da2b792
Ryan A. Novelli <ryan.a.novelli@gmail.com>
2022-01-18 14:08:52 -0800

    updates index and conversion for decimal num usage

--- conversion.php
+++ conversion.php
@@ -1,73 +1,49 @@
 <?php
-
-function fahrenheit_to_celsius($temp)
+class TemperatureConversion
 {
-    if (gettype($temp == 'int')) {
-        $celsius = 5 / 9 * ($temp - 32);
-        $celsius = round($celsius, 2);
-        return $celsius;
-    } else {
-        $message = 'Please enter an integer';
-        return $message;
+    public $temp;
+    public $type;
+    // Methods
+    function set_Temperature($Temperature)
+    {
+        $this->temp = $Temperature;
     }
-}
-
-function fahrenheit_to_kelvin($temp)
-{
-    if (gettype($temp == 'int')) {
-        $kelvin = 5 / 9 * ($temp - 32) + 273;
-        $kelvin = round($kelvin, 2);
-        return $kelvin;
-    } else {
-        $message = 'Please enter an integer';
-        return $message;
+    function get_Temperature()
+    {
+        return $this->temp;
     }
-}
 
-function celsius_to_fahrenheit($temp)
-{
-    if (gettype($temp == 'int')) {
-        $fahrenheit = 9 / 5 * ($temp) + 32;
-        $fahrenheit = round($fahrenheit, 2);
-        return $fahrenheit;
-    } else {
-        $message = 'Please enter an integer';
-        return $message;
+    function set_ConvertType($ConvertType)
+    {
+        $this->type = $ConvertType;
     }
-}
-
-function celsius_to_kelvin($temp)
-{
-    if (gettype($temp == 'int')) {
-        $kelvin = $temp + 273;
-        $kelvin = round($kelvin, 2);
-        return $kelvin;
-    } else {
-        $message = 'Please enter an integer';
-        return $message;
+    function get_ConvertType()
+    {
+        return $this->type;
     }
-}
 
-function kelvin_to_fahrenheit($temp)
-{
-    if (gettype($temp == 'int')) {
-        $fahrenheit = 9 / 5 * ($temp - 273) + 32;
-        $fahrenheit = round($fahrenheit, 2);
-        return $fahrenheit;
-    } else {
-        $message = 'Please enter an integer';
-        return $message;
-    }
-}
-
-function kelvin_to_celsius($temp)
-{
-    if (gettype($temp == 'int')) {
-        $celsius = $temp - 273;
-        $celsius = round($celsius, 2);
-        return $celsius;
-    } else {
-        $message = 'Please enter an integer';
-        return $message;
+    function get_convertResult()
+    {
+        switch ($this->type) {
+            case 'CtoF':
+                $this->result = number_format(9 / 5 * ($this->temp) + 32, 2);
+                break;
+            case 'CtoK':
+                $this->result = number_format($this->temp + 273, 2);
+                break;
+            case 'FtoC':
+                $this->result = number_format(5 / 9 * ($this->temp - 32), 2);
+                break;
+            case 'FtoK':
+                $this->result = number_format(5 / 9 * ($this->temp - 32) + 273, 2);
+                break;
+            case 'KtoC':
+                $this->result = number_format($this->temp - 273, 2);
+                break;
+            case 'KtoF':
+                $this->result = number_format(9 / 5 * ($this->temp - 273) + 32, 2);
+                break;
+        }
+        return $this->result;
     }
 }
